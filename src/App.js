755733178e1dd3f09c6676b565d830908462ce97
NguyenDaiDuong <daiduong28.11.93@gmail.com>
2019-08-05 16:59:21 +0700

    DuAnWebXemPhimReact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Header from './common/components/Header/Header';
+import HomePage from './pages/HomePage/HomePage';
+import CourseList from './pages/CourseList/CourseList';
+import CourseDetails from './pages/CourseDetails/CourseDetails';
+import HomeTemplate from './templates/HomeTemplate';
+import Demo from './common/components/Demo';
+import Login from './pages/Login/Login';
+import TrangChu from './pages/DuAnDatVePhim/TrangChu';
+import TrangChiTiet from './pages/DuAnDatVePhim/TrangChiTiet';
+import TrangDatVe from './pages/DuAnDatVePhim/TrangDatVe';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Switch>
+          {/* <HomeTemplate  path='/home'  Component={HomePage}></HomeTemplate>
+          <HomeTemplate  path='/courselist'  Component={CourseList}></HomeTemplate>
+          <HomeTemplate  path='/coursedetails/:courseid'  Component={CourseDetails}></HomeTemplate>
+          <HomeTemplate  path='/login'  Component={Login}></HomeTemplate>
+          <HomeTemplate  path='/'  Component={HomePage}></HomeTemplate> */}
+          <HomeTemplate path='/trang-chu' Component={TrangChu} ></HomeTemplate>
+          <HomeTemplate path='/chi-tiet-phim/:maPhim' Component={TrangChiTiet} ></HomeTemplate>
+          <HomeTemplate path='/chi-tiet-phong-ve/:maLichChieu' Component={TrangDatVe} ></HomeTemplate>
+          <HomeTemplate path='/'  Component={TrangChu}></HomeTemplate>
+          
+        </Switch>
+      </Fragment>
+    </BrowserRouter>
+
+
   );
 }
 
